47ee6f9d2510fe05350dd0b16a02c434557eed23
itheima <jskdlajklsadfklj>
2018-12-25 21:40:38 +0800

    第一次储存

---
+++ lingshixiaolianxi/src/main/java/com/itheima/aaa/aa.java
@@ -0,0 +1,10 @@
+package com.itheima.aaa;
+
+public class aa {
+    public static void main(String[] args) {
+        System.out.println("56415641564");
+        System.out.println("56415641564");
+        System.out.println("56415641564");
+        System.out.println("56415641564");
+    }
+}
